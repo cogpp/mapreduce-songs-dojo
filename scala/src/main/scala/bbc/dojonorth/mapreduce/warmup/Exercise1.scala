f287d22941e55c20aa767e3e168efa4e436ebb90
Graeme Phillipson <graeme.phillipson@bbc.co.uk>
2015-06-12 14:40:15 +0100

    Exercise1 solutions

--- scala/src/main/scala/bbc/dojonorth/mapreduce/warmup/Exercise1.scala
+++ scala/src/main/scala/bbc/dojonorth/mapreduce/warmup/Exercise1.scala
@@ -2,13 +2,13 @@ package bbc.dojonorth.mapreduce.warmup
 
 class Exercise1 {
 
-  def sumOfList(xs: List[Int]): Int = ???
+  def sumOfList(xs: List[Int]): Int = xs.sum
 
-  def sumOfSquaresOfList(xs: List[Int]): Int = ???
+  def sumOfSquaresOfList(xs: List[Int]): Int = xs map(x => x*x) sum
 
-  def productOfDoubleOfSquaresOfList(xs: List[Int]): Int = ???
+  def productOfDoubleOfSquaresOfList(xs: List[Int]): Int = xs map (x=> 2*x*x) reduceOption (_ * _) getOrElse 0
 
   // mapReduce is a higher-order function
-  def mapReduce(mapFn: (Int => Int), reduceFun: (Int,Int) => Int, input: List[Int]): Int = ???
+  def mapReduce(mapFn: (Int => Int), reduceFun: (Int,Int) => Int, input: List[Int]): Int = input map mapFn reduce reduceFun
 
 }
